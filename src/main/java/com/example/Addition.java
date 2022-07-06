10de3b3e703713320448bf790c94eee4664e2f25
Satya Nayani Ravanam <nayaniravanam@gmail.com>
2022-07-06 15:40:27 +0530

    firstcommit

---
+++ src/main/java/com/example/Addition.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=34;
+		b=90;
+		c=a+b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/main/java/com/example/AdditionRuntime.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class AdditionRuntime {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("enter the a value");
+		a=scan.nextInt();
+		System.out.println("enter b value");
+		b=scan.nextInt();
+		c=a+b;
+		System.out.println(c);
+		
+
+	}
+
+}
